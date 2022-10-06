e1be7711d33cc402af0059a46c0253957f5c08b1
JD_Rodrigues <jdevproject01@gmail.com>
2022-10-06 14:40:32 -0300

    Adição do botão play/pause no card de radio

--- src/components/audio/index.tsx
+++ src/components/audio/index.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react"
 import { IAudioProps } from "../../types"
 import styles from "./styles.module.css"
 
 
 export const Player = ({url, poster}:IAudioProps) => {
+const [playing, setPlaying] = useState(false)
+
   return(
     <div className={styles.card__player}>
       <div className={styles.poster} style={{backgroundImage: `url("${poster}")`}}>
         <audio preload="auto">
           <source src={url} type="audio/mp3" />
         </audio>
+        <div className={styles.play__pause} onClick={()=>setPlaying(true)}>
+            <img src={playing ? "/icons/pause.png" : "/icons/play.png"}></img>
+        </div>
       </div>
       
     </div>
