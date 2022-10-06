82b9b382a85fb49e38d2b62c590a63e4831142f9
JD_Rodrigues <jdevproject01@gmail.com>
2022-10-06 16:00:23 -0300

    Implementação da troca de ícones no menu play/pause, de acordo com o estado da reprodução - roda girando enquanto em reprodução

--- src/components/audio/index.tsx
+++ src/components/audio/index.tsx
@@ -12,8 +12,9 @@ const [playing, setPlaying] = useState(false)
         <audio preload="auto">
           <source src={url} type="audio/mp3" />
         </audio>
-        <div className={styles.play__pause} onClick={()=>setPlaying(true)}>
-            <img src={playing ? "/icons/pause.png" : "/icons/play.png"}></img>
+        <div className={styles.play__pause} onClick={()=>setPlaying(prevState=>!prevState)}>
+            <img className={styles.spinning__wheel} src="/icons/wheel.png"></img>
+            <img src={playing ? "" : "/icons/play.png"}></img>
         </div>
       </div>
       
